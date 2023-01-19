8a5f6e62fe2ce4147abfec2899978af532a2501f
Saul <saul_henriquez@hotmail.com>
2023-01-19 07:08:16 -0600

    Restore FlowerPot
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/util/OmegaUtils.kt
+++ Omega/src/com/saggitt/omega/util/OmegaUtils.kt
@@ -18,4 +18,41 @@
 
 package com.saggitt.omega.util
 
+import android.os.Looper
+import com.android.launcher3.util.Executors.MAIN_EXECUTOR
+import java.util.Locale
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutionException
+
+fun <T, A> ensureOnMainThread(creator: (A) -> T): (A) -> T {
+    return { it ->
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            creator(it)
+        } else {
+            try {
+                MAIN_EXECUTOR.submit(Callable { creator(it) }).get()
+            } catch (e: InterruptedException) {
+                throw RuntimeException(e)
+            } catch (e: ExecutionException) {
+                throw RuntimeException(e)
+            }
+
+        }
+    }
+}
+
+fun String.toTitleCase(): String = splitToSequence(" ").map {
+    it.replaceFirstChar { ch ->
+        if (ch.isLowerCase()) ch.titlecase(
+            Locale.getDefault()
+        ) else ch.toString()
+    }
+}.joinToString(" ")
+
+inline fun <T> listWhileNotNull(generator: () -> T?): List<T> = mutableListOf<T>().apply {
+    while (true) {
+        add(generator() ?: break)
+    }
+}
+
 val isBlackTheme: Boolean = false //TODO add black theme support
