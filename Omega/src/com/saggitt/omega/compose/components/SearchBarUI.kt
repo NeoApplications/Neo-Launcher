efb5bf491fb9923e30af9dc4057b25e65859a5bf
Saul Henriquez <saul_henriquez@hotmail.com>
2023-01-23 17:57:41 -0600

    Restore SearchBarUI
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ Omega/src/com/saggitt/omega/compose/components/SearchBarUI.kt
@@ -0,0 +1,131 @@
+/*
+ * This file is part of Neo Launcher
+ * Copyright (c) 2023   Neo Launcher Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.compose.components
+
+
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.android.launcher3.R
+import com.google.accompanist.insets.ui.LocalScaffoldPadding
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBarUI(
+    searchInput: @Composable () -> Unit,
+    actions: @Composable RowScope.() -> Unit = {},
+    content: @Composable (PaddingValues) -> Unit
+) {
+    val searchBarHeight = 56.dp
+    val innerPadding = remember { MutablePaddingValues() }
+    val searchBarVerticalMargin = 8.dp
+    val statusBarHeight = 26.dp
+    val contentShift = statusBarHeight + searchBarVerticalMargin + searchBarHeight / 2
+    val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+
+    Scaffold(
+        topBar = {
+            Surface(
+                modifier = Modifier
+                    .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Top))
+                    .padding(horizontal = 16.dp, vertical = searchBarVerticalMargin)
+                    .height(searchBarHeight),
+                shape = MaterialTheme.shapes.small,
+                shadowElevation = 2.dp
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(searchBarHeight)
+                        .padding(horizontal = 4.dp),
+                    contentAlignment = Alignment.CenterStart,
+                ) {
+                    IconButton(
+                        onClick = { backDispatcher?.onBackPressed() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.gesture_press_back)
+                        )
+                    }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 36.dp)
+                    ) {
+                        Box(modifier = Modifier.weight(1f)) {
+                            searchInput()
+                        }
+                        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                            Row(
+                                Modifier.fillMaxHeight(),
+                                horizontalArrangement = Arrangement.End,
+                                verticalAlignment = Alignment.CenterVertically,
+                                content = actions
+                            )
+                        }
+                    }
+                }
+            }
+        },
+        bottomBar = { BottomSpacer() }
+    ) {
+        val layoutDirection = LocalLayoutDirection.current
+        innerPadding.left = it.calculateLeftPadding(layoutDirection)
+        innerPadding.top = it.calculateTopPadding() - contentShift
+        innerPadding.right = it.calculateRightPadding(layoutDirection)
+        innerPadding.bottom = it.calculateBottomPadding()
+        CompositionLocalProvider(
+            LocalScaffoldPadding provides innerPadding
+        ) {
+            Box(modifier = Modifier.padding(top = contentShift)) {
+                content(it)
+            }
+        }
+    }
+}
+
+@Stable
+internal class MutablePaddingValues : PaddingValues {
+    var left: Dp by mutableStateOf(0.dp)
+    var top: Dp by mutableStateOf(0.dp)
+    var right: Dp by mutableStateOf(0.dp)
+    var bottom: Dp by mutableStateOf(0.dp)
+
+    override fun calculateLeftPadding(layoutDirection: LayoutDirection) = left
+
+    override fun calculateTopPadding(): Dp = top
+
+    override fun calculateRightPadding(layoutDirection: LayoutDirection) = right
+
+    override fun calculateBottomPadding(): Dp = bottom
+}
