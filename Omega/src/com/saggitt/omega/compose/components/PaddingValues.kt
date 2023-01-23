bd06fdfa444fd2526b67289eaeae3cdda409aecb
Saul Henriquez <saul_henriquez@hotmail.com>
2023-01-23 17:52:44 -0600

    Restore PaddingValues
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ Omega/src/com/saggitt/omega/compose/components/PaddingValues.kt
@@ -0,0 +1,92 @@
+/*
+ * This file is part of Neo Launcher
+ * Copyright (c) 2023   Neo Launcher Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.compose.components
+
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun rememberExtendPadding(
+    padding: PaddingValues,
+    start: Dp = 0.dp,
+    top: Dp = 0.dp,
+    end: Dp = 0.dp,
+    bottom: Dp = 0.dp
+): PaddingValues = remember(start, top, end, bottom) {
+    CombinePaddingValues(
+        padding, PaddingValues(
+            start = start,
+            top = top,
+            end = end,
+            bottom = bottom
+        )
+    )
+}
+
+private class CombinePaddingValues(private val a: PaddingValues, private val b: PaddingValues) :
+    PaddingValues {
+    override fun calculateLeftPadding(layoutDirection: LayoutDirection) =
+        a.calculateLeftPadding(layoutDirection) + b.calculateLeftPadding(layoutDirection)
+
+    override fun calculateTopPadding() =
+        a.calculateTopPadding() + b.calculateTopPadding()
+
+    override fun calculateRightPadding(layoutDirection: LayoutDirection) =
+        a.calculateRightPadding(layoutDirection) + b.calculateRightPadding(layoutDirection)
+
+    override fun calculateBottomPadding() =
+        a.calculateBottomPadding() + b.calculateBottomPadding()
+}
+
+@Composable
+operator fun PaddingValues.minus(b: PaddingValues): PaddingValues {
+    val a = this
+    return remember(a, b) {
+        object : PaddingValues {
+            override fun calculateLeftPadding(layoutDirection: LayoutDirection): Dp {
+                val aLeft = a.calculateLeftPadding(layoutDirection)
+                val bLeft = b.calculateRightPadding(layoutDirection)
+                return (aLeft - bLeft).coerceAtLeast(0.dp)
+            }
+
+            override fun calculateTopPadding(): Dp {
+                val aTop = a.calculateTopPadding()
+                val bTop = b.calculateTopPadding()
+                return (aTop - bTop).coerceAtLeast(0.dp)
+            }
+
+            override fun calculateRightPadding(layoutDirection: LayoutDirection): Dp {
+                val aRight = a.calculateRightPadding(layoutDirection)
+                val bRight = b.calculateRightPadding(layoutDirection)
+                return (aRight - bRight).coerceAtLeast(0.dp)
+            }
+
+            override fun calculateBottomPadding(): Dp {
+                val aBottom = a.calculateBottomPadding()
+                val bBottom = b.calculateBottomPadding()
+                return (aBottom - bBottom).coerceAtLeast(0.dp)
+            }
+        }
+    }
+}
