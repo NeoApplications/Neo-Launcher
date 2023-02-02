0fc1a074713de8f19a4c9563df4cc66fe5607881
machiav3lli <antonios.hazim@tutanota.com>
2023-02-02 03:52:22 +0100

    Add: (Web)Search Providers

---
+++ Omega/src/com/saggitt/omega/search/SearchProvider.kt
@@ -0,0 +1,62 @@
+/*
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.search
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.Drawable
+
+abstract class SearchProvider(protected val context: Context) {
+    abstract val name: String
+    abstract val supportsVoiceSearch: Boolean
+    abstract val supportsAssistant: Boolean
+    abstract val supportsFeed: Boolean
+
+    abstract val packageName: String
+    abstract val icon: Drawable
+    abstract val iconRes: Int
+    open val voiceIcon: Drawable?
+        get() = if (supportsVoiceSearch)
+            throw RuntimeException("Voice search supported but not implemented")
+        else null
+    open val assistantIcon: Drawable?
+        get() = if (supportsVoiceSearch)
+            throw RuntimeException("Assistant supported but not implemented")
+        else null
+
+    /**
+     * Whether the settings intent needs to be sent as broadcast
+     */
+    open val isBroadcast = false
+    open val isAvailable: Boolean = true
+
+    abstract fun startSearch(callback: (intent: Intent) -> Unit = {})
+    open fun startVoiceSearch(callback: (intent: Intent) -> Unit = {}) {
+        if (supportsVoiceSearch) throw RuntimeException("Voice search supported but not implemented")
+    }
+
+    open fun startAssistant(callback: (intent: Intent) -> Unit = {}) {
+        if (supportsAssistant) throw RuntimeException("Assistant supported but not implemented")
+    }
+
+    open fun startFeed(callback: (intent: Intent) -> Unit = {}) {
+        if (supportsFeed) throw RuntimeException("Feed supported but not implemented")
+    }
+
+    override fun toString(): String = this::class.java.name
+}
---
+++ Omega/src/com/saggitt/omega/search/provider/GoogleSearchProvider.kt
@@ -0,0 +1,78 @@
+/*
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.search.provider
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.annotation.Keep
+import androidx.core.content.res.ResourcesCompat
+import com.android.launcher3.R
+import com.saggitt.omega.search.GOOGLE_QSB
+import com.saggitt.omega.search.SearchProvider
+import com.saggitt.omega.util.isAppEnabled
+
+@Keep
+class GoogleSearchProvider(context: Context) : SearchProvider(context) {
+
+    override val name = context.getString(R.string.google_app)
+    override val supportsVoiceSearch = true
+    override val supportsAssistant = true
+    override val isAvailable: Boolean
+        get() = context.packageManager.isAppEnabled(GOOGLE_QSB, 0)
+    override val supportsFeed = true
+
+    override val packageName: String
+        get() = GOOGLE_QSB
+    override val isBroadcast: Boolean
+        get() = true
+
+    override fun startSearch(callback: (intent: Intent) -> Unit) =
+        callback(Intent().setClassName(GOOGLE_QSB, "$GOOGLE_QSB.SearchActivity"))
+
+    override fun startVoiceSearch(callback: (intent: Intent) -> Unit) =
+        callback(
+            Intent("android.intent.action.VOICE_ASSIST").addFlags(268468224)
+                .setPackage(GOOGLE_QSB)
+        )
+
+    override fun startAssistant(callback: (intent: Intent) -> Unit) =
+        callback(
+            Intent(Intent.ACTION_VOICE_COMMAND).addFlags(268468224).setPackage(GOOGLE_QSB)
+        )
+
+    override fun startFeed(callback: (intent: Intent) -> Unit) {
+        callback(
+            Intent(Intent.ACTION_MAIN).setClassName(
+                GOOGLE_QSB,
+                "$GOOGLE_QSB.SearchActivity"
+            )
+        )
+    }
+
+    override val iconRes: Int
+        get() = R.drawable.ic_super_g_color
+    override val icon: Drawable
+        get() = ResourcesCompat.getDrawable(context.resources, iconRes, null)!!
+
+    override val voiceIcon: Drawable
+        get() = ResourcesCompat.getDrawable(context.resources, R.drawable.ic_mic_color, null)!!
+
+    override val assistantIcon: Drawable
+        get() = ResourcesCompat.getDrawable(context.resources, R.drawable.ic_qsb_assist, null)!!
+}
