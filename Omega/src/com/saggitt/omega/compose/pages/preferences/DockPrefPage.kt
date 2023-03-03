b8f2ac1fe75c4daa5ec6b0892e29df0a5f973054
machiav3lli <antonios.hazim@tutanota.com>
2023-03-03 01:49:54 +0100

    Add: Dock prefs page

--- Omega/src/com/saggitt/omega/compose/pages/preferences/DockPrefPage.kt
+++ Omega/src/com/saggitt/omega/compose/pages/preferences/DockPrefPage.kt
@@ -18,8 +18,102 @@
 
 package com.saggitt.omega.compose.pages.preferences
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraphBuilder
+import com.android.launcher3.R
+import com.saggitt.omega.compose.components.BaseDialog
+import com.saggitt.omega.compose.components.ViewWithActionBar
+import com.saggitt.omega.compose.components.preferences.IntSelectionPrefDialogUI
+import com.saggitt.omega.compose.components.preferences.PreferenceGroup
+import com.saggitt.omega.compose.components.preferences.StringMultiSelectionPrefDialogUI
+import com.saggitt.omega.compose.components.preferences.StringSelectionPrefDialogUI
+import com.saggitt.omega.compose.navigation.Routes
+import com.saggitt.omega.compose.navigation.preferenceGraph
+import com.saggitt.omega.preferences.IntSelectionPref
+import com.saggitt.omega.preferences.PrefKey
+import com.saggitt.omega.preferences.StringMultiSelectionPref
+import com.saggitt.omega.preferences.StringSelectionPref
+import com.saggitt.omega.theme.OmegaAppTheme
+import com.saggitt.omega.util.prefs
 
 @Composable
-fun DockPrefPage() {
+fun DockPrefsPage() {
+    val context = LocalContext.current
+    val prefs = context.prefs
+    val openDialog = remember { mutableStateOf(false) }
+    var dialogPref by remember { mutableStateOf<Any?>(null) }
+    val onPrefDialog = { pref: Any ->
+        dialogPref = pref
+        openDialog.value = true
+    }
+
+    val dockPrefs = listOf(
+        prefs.dockBackgroundColor,
+    )
+
+    OmegaAppTheme {
+        ViewWithActionBar(
+            title = stringResource(R.string.title__general_desktop)
+        ) { paddingValues ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 8.dp),
+                contentPadding = paddingValues,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                item {
+                    PreferenceGroup(
+                        prefs = dockPrefs,
+                        onPrefDialog = onPrefDialog
+                    )
+                }
+            }
+
+            if (openDialog.value) {
+                BaseDialog(openDialogCustom = openDialog) {
+                    when (dialogPref) {
+                        is IntSelectionPref         -> IntSelectionPrefDialogUI(
+                            pref = dialogPref as IntSelectionPref,
+                            openDialogCustom = openDialog
+                        )
+
+                        is StringSelectionPref      -> StringSelectionPrefDialogUI(
+                            pref = dialogPref as StringSelectionPref,
+                            openDialogCustom = openDialog
+                        )
+
+                        is StringMultiSelectionPref -> StringMultiSelectionPrefDialogUI(
+                            pref = dialogPref as StringMultiSelectionPref,
+                            openDialogCustom = openDialog
+                        )
+                        /*is GridSize -> GridSizePrefDialogUI(
+                            pref = dialogPref as GridSize,
+                            openDialogCustom = openDialog
+                        )*/
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun NavGraphBuilder.dockPrefsGraph(route: String) {
+    preferenceGraph(route, { DockPrefsPage() }) { subRoute ->
+        preferenceGraph(
+            route = subRoute(Routes.COLOR_BG_DOCK),
+            { ColorSelectorPage(PrefKey.DOCK_BG_COLOR) })
+    }
 }
