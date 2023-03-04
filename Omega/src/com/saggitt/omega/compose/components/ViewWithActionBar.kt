8f81eae28fc22c2e9c2c3eeb6d7ec082e294058a
Saul Henriquez <saul_henriquez@hotmail.com>
2023-03-03 19:08:03 -0600

    Fix: Dock page title
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/compose/components/ViewWithActionBar.kt
+++ Omega/src/com/saggitt/omega/compose/components/ViewWithActionBar.kt
@@ -32,8 +32,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.android.launcher3.R
+import com.android.launcher3.Utilities
+import com.saggitt.omega.util.addIf
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,6 +52,7 @@ fun ViewWithActionBar(
     bottomBar: @Composable () -> Unit = {},
     content: @Composable (PaddingValues) -> Unit
 ) {
+    val prefs = Utilities.getOmegaPrefs(LocalContext.current)
     Scaffold(
         topBar = {
             TopAppBar(
@@ -77,6 +84,9 @@ fun ViewWithActionBar(
         },
         floatingActionButton = floatingActionButton,
         content = content,
-        bottomBar = bottomBar
+        bottomBar = bottomBar,
+        modifier = Modifier.addIf(prefs.profileBlurEnable.getValue()) {
+            blur(prefs.profileBlurRadius.getValue().dp)
+        }
     )
 }
