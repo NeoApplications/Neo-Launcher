73963b5607026b7d00e51ddb84b5a097bc194d28
Saul Henriquez <saul_henriquez@hotmail.com>
2023-03-05 20:57:38 -0600

    Disable desktop mode
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/compose/pages/preferences/MainPrefsPage.kt
+++ Omega/src/com/saggitt/omega/compose/pages/preferences/MainPrefsPage.kt
@@ -61,7 +61,7 @@ fun MainPrefsPage() {
     )
     val otherPrefs = listOfNotNull(
         PageItem.PrefsBackup,
-        PageItem.PrefsDesktopMode,
+        //PageItem.PrefsDesktopMode,
         if (prefs.developerOptionsEnabled.getValue()) PageItem.PrefsDeveloper
         else null,
         PageItem.PrefsAbout
