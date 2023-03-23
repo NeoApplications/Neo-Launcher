56ce5f292643b125448f8a02139315b2a8836c69
Saul Henriquez <saul_henriquez@hotmail.com>
2023-03-22 20:42:48 -0600

    Move DoubleShadowTextView to superg package
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ Omega/src/com/saggitt/omega/smartspace/superg/DoubleShadowTextView.kt
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Neo Launcher
+ * Copyright (c) 2023   Neo Launcher Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saggitt.omega.smartspace.superg
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.TextView
+import com.android.launcher3.views.DoubleShadowBubbleTextView.ShadowInfo
+import kotlin.math.max
+
+open class DoubleShadowTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    TextView(context, attrs, defStyleAttr) {
+    private val mShadowInfo: ShadowInfo
+
+    init {
+        mShadowInfo = ShadowInfo(context, attrs, defStyleAttr)
+        setShadowLayer(
+            max(
+                mShadowInfo.keyShadowBlur + mShadowInfo.keyShadowOffsetX,
+                mShadowInfo.ambientShadowBlur
+            ), 0f, 0f, mShadowInfo.keyShadowColor
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (mShadowInfo.skipDoubleShadow(this)) {
+            super.onDraw(canvas)
+            return
+        }
+        paint.setShadowLayer(
+            mShadowInfo.ambientShadowBlur,
+            0.0f,
+            0.0f,
+            mShadowInfo.ambientShadowColor
+        )
+        super.onDraw(canvas)
+        paint.setShadowLayer(
+            mShadowInfo.keyShadowBlur,
+            0.0f,
+            mShadowInfo.keyShadowOffsetX,
+            mShadowInfo.keyShadowColor
+        )
+        super.onDraw(canvas)
+    }
+}
