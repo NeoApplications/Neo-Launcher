22f311ef5267b9362d32ed7dd922129d8db8ba88
machiav3lli <antonios.hazim@tutanota.com>
2023-05-19 03:51:19 +0200

    Update: Revamp Pager bar item layout

--- Omega/src/com/saggitt/omega/compose/components/Pager.kt
+++ Omega/src/com/saggitt/omega/compose/components/Pager.kt
@@ -1,21 +1,34 @@
 package com.saggitt.omega.compose.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
-import androidx.compose.material.TabRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Tab
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.saggitt.omega.util.blockBorder
 import kotlinx.coroutines.launch
 
@@ -40,42 +53,65 @@ fun HorizontalPagerPage(
         ) { page ->
             tabs[page].screen()
         }
-        TabRow(
-            selectedTabIndex = pagerState.currentPage,
-            indicator = {},
-            divider = {},
-            backgroundColor = MaterialTheme.colorScheme.background
+        NavigationBar(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.onBackground,
         ) {
             tabs.forEachIndexed { index, tab ->
-                Tab(
+                NavBarItem(
+                    modifier = Modifier.weight(1f),
+                    icon = painterResource(id = tab.icon),
+                    labelId = tab.title,
                     selected = pagerState.currentPage == index,
                     onClick = {
                         coroutineScope.launch {
                             pagerState.animateScrollToPage(index)
                         }
                     },
-                    text = {
-                        Text(
-                            text = stringResource(id = tab.title),
-                            color = if (pagerState.currentPage == index)
-                                MaterialTheme.colorScheme.primary
-                            else
-                                MaterialTheme.colorScheme.onPrimary
-                        )
-                    },
-                    icon = {
-                        Icon(
-                            painter = painterResource(id = tab.icon),
-                            contentDescription = stringResource(id = tab.title),
-                            tint = if (pagerState.currentPage == index)
-                                MaterialTheme.colorScheme.primary
-                            else
-                                MaterialTheme.colorScheme.onPrimary
-                        )
-                    }
-
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun NavBarItem(
+    modifier: Modifier = Modifier,
+    icon: Painter,
+    labelId: Int,
+    selected: Boolean,
+    onClick: () -> Unit = {},
+) {
+    Row(
+        modifier = modifier.clickable { onClick() },
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = stringResource(id = labelId),
+                modifier = Modifier
+                    .background(
+                        if (selected) MaterialTheme.colorScheme.surfaceColorAtElevation(48.dp)
+                        else Color.Transparent,
+                        CircleShape
+                    )
+                    .padding(8.dp)
+                    .size(if (selected) 36.dp else 26.dp),
+                tint = if (selected) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.onSurface
+            )
+            if (!selected)
+                Text(
+                    text = stringResource(id = labelId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
         }
     }
 }
