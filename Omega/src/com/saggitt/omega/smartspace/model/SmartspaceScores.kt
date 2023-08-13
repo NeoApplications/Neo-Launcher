4d983872fd19ae600ab0e7a8a3f381fc8046766c
Saul Henriquez <saul_henriquez@hotmail.com>
2023-08-13 07:31:37 -0600

    Reformat SmartspaceScores
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- Omega/src/com/saggitt/omega/smartspace/model/SmartspaceScores.kt
+++ Omega/src/com/saggitt/omega/smartspace/model/SmartspaceScores.kt
@@ -1,8 +1,9 @@
 package com.saggitt.omega.smartspace.model
 
 object SmartspaceScores {
-    const val SCORE_WEATHER = 0f
-    const val SCORE_BATTERY = 1f
-    const val SCORE_MEDIA = 2f
-    const val SCORE_CALENDAR = 3f
+    const val SCORE_WEATHER = 0
+    const val SCORE_BATTERY = 1
+    const val SCORE_MEDIA = 2
+    const val SCORE_CALENDAR = 3
+    const val SCORE_ALARM = 4
 }
