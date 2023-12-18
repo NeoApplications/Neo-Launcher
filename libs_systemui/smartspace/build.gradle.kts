f3f01cce801ed4c775ee2783bbb8c966dd22512e
machiav3lli <antonios.hazim@tutanota.com>
2023-12-18 22:33:35 +0100

    Update Dependencies (System UI/libs)

--- libs_systemui/smartspace/build.gradle.kts
+++ libs_systemui/smartspace/build.gradle.kts
@@ -46,6 +46,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
     implementation("androidx.annotation:annotation:1.7.0")
 }
