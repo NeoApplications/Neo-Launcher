732d1475ae1972f773746e3ef6401f686fffb146
machiav3lli <antonios.hazim@tutanota.com>
2024-09-25 14:26:45 +0200

    Update: Replace deprecated call

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import com.android.build.gradle.internal.tasks.factory.dependsOn
+import org.jetbrains.kotlin.utils.addIfNotNull
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.TimeZone
@@ -311,9 +312,9 @@ task("detectAndroidLocals") {
         if (this.file.path.endsWith("strings.xml")
             && this.file.canonicalFile.readText().contains("<string")
         ) {
-            var languageCode = this.file.parentFile.name.replace("values-", "")
+            var languageCode = this.file.parentFile?.name?.replace("values-", "")
             languageCode = if (languageCode == "values") "en" else languageCode
-            langsList.add(languageCode)
+            langsList.addIfNotNull(languageCode)
         }
     }
     val langsListString = "{${langsList.joinToString(",") { "\"${it}\"" }}}"
