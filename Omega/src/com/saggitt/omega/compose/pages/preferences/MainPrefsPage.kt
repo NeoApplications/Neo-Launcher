8ba5e2ee132fe6376226475b10d6ac957c433be7
machiav3lli <antonios.hazim@tutanota.com>
2024-09-25 14:04:38 +0200

    Add: Adaptive layout to settings

--- Omega/src/com/saggitt/omega/compose/pages/preferences/MainPrefsPage.kt
+++ Omega/src/com/saggitt/omega/compose/pages/preferences/MainPrefsPage.kt
@@ -22,57 +22,79 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavGraphBuilder
 import com.android.launcher3.BuildConfig
 import com.android.launcher3.R
 import com.android.launcher3.Utilities
 import com.saggitt.omega.changeDefaultHome
 import com.saggitt.omega.compose.components.OverflowMenu
 import com.saggitt.omega.compose.components.ViewWithActionBar
+import com.saggitt.omega.compose.components.plus
 import com.saggitt.omega.compose.components.preferences.PreferenceGroup
-import com.saggitt.omega.compose.navigation.LocalNavController
-import com.saggitt.omega.compose.navigation.Routes
-import com.saggitt.omega.compose.navigation.preferenceGraph
-import com.saggitt.omega.compose.navigation.subRoute
+import com.saggitt.omega.compose.navigation.LocalPaneNavigator
+import com.saggitt.omega.compose.navigation.NavRoute
 import com.saggitt.omega.compose.objects.PageItem
-import com.saggitt.omega.compose.pages.editIconGraph
+import com.saggitt.omega.compose.pages.AppCategoriesPage
+import com.saggitt.omega.compose.pages.ColorSelectionPage
+import com.saggitt.omega.compose.pages.GestureSelectorPage
+import com.saggitt.omega.compose.pages.HiddenAppsPage
+import com.saggitt.omega.compose.pages.IconShapePage
+import com.saggitt.omega.compose.pages.ProtectedAppsPage
+import com.saggitt.omega.compose.pages.ProtectedAppsView
+import com.saggitt.omega.preferences.PrefKey
+import kotlinx.collections.immutable.persistentListOf
 
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 fun MainPrefsPage() {
     val context = LocalContext.current
+    val paneNavigator = LocalPaneNavigator.current
     val prefs = Utilities.getNeoPrefs(context)
-    val uiPrefs = listOf(
+
+    val uiPrefs = persistentListOf(
         PageItem.PrefsProfile,
         PageItem.PrefsDesktop,
         PageItem.PrefsDock,
         PageItem.PrefsDrawer
     )
-    val featuresPrefs = listOf(
+    val featuresPrefs = persistentListOf(
         PageItem.PrefsWidgetsNotifications,
         PageItem.PrefsSearchFeed,
         PageItem.PrefsGesturesDash
     )
-    val otherPrefs = listOfNotNull(
+    val otherPrefs: List<PageItem> = listOfNotNull(
         //PageItem.PrefsBackup,
         //PageItem.PrefsDesktopMode,
         if (prefs.developerOptionsEnabled.getValue()) PageItem.PrefsDeveloper
         else null,
         PageItem.PrefsAbout
     )
-    val navController = LocalNavController.current
-    val destination = subRoute(Routes.PREFS_DEV)
+    val gesturesMap = listOf(
+        prefs.gestureDoubleTap,
+        prefs.gestureLongPress,
+        prefs.gestureSwipeDown,
+        prefs.gestureSwipeUp,
+        prefs.gestureDockSwipeUp,
+        prefs.gestureHomePress,
+        prefs.gestureBackPress,
+    ).associateBy { it.key.name }
+    val pageData: MutableState<NavRoute?> = remember { mutableStateOf(null) }
 
     fun resolveDefaultHome(): String? {
         val homeIntent: Intent = Intent(Intent.ACTION_MAIN)
@@ -109,7 +131,7 @@ fun MainPrefsPage() {
                 )
                 DropdownMenuItem(
                     onClick = {
-                        navController.navigate(destination)
+                        paneNavigator.navigateTo(ListDetailPaneScaffoldRole.Detail, NavRoute.Dev())
                         hideMenu()
                     },
                     text = { Text(text = stringResource(id = R.string.developer_options_title)) }
@@ -117,50 +139,133 @@ fun MainPrefsPage() {
             }
         }
     ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 8.dp),
-            contentPadding = paddingValues,
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            item {
-                PreferenceGroup(
-                    heading = stringResource(id = R.string.pref_category__interfaces),
-                    prefs = uiPrefs
-                )
-            }
-            item {
-                PreferenceGroup(
-                    heading = stringResource(id = R.string.pref_category__features),
-                    prefs = featuresPrefs
-                )
-            }
-            item {
-                PreferenceGroup(
-                    heading = stringResource(id = R.string.pref_category__others),
-                    prefs = otherPrefs
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-        }
-    }
 
-}
+        NavigableListDetailPaneScaffold(
+            navigator = paneNavigator,
+            listPane = {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = paddingValues + PaddingValues(
+                        top = 8.dp,
+                        start = 8.dp,
+                        end = 8.dp
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    item {
+                        PreferenceGroup(
+                            heading = stringResource(id = R.string.pref_category__interfaces),
+                            prefs = uiPrefs
+                        )
+                    }
+                    item {
+                        PreferenceGroup(
+                            heading = stringResource(id = R.string.pref_category__features),
+                            prefs = featuresPrefs
+                        )
+                    }
+                    item {
+                        PreferenceGroup(
+                            heading = stringResource(id = R.string.pref_category__others),
+                            prefs = otherPrefs
+                        )
+                    }
+                }
+            },
+            detailPane = {
+                pageData.value = paneNavigator.currentDestination
+                    ?.takeIf { it.pane == this.role }?.content
+                    ?.let { it as? NavRoute }
 
-fun NavGraphBuilder.mainPrefsGraph(route: String) {
-    preferenceGraph(route, { MainPrefsPage() }) { subRoute ->
-        profilePrefsGraph(route = subRoute(Routes.PREFS_PROFILE))
-        desktopPrefsGraph(route = subRoute(Routes.PREFS_DESKTOP))
-        dockPrefsGraph(route = subRoute(Routes.PREFS_DOCK))
-        drawerPrefsGraph(route = subRoute(Routes.PREFS_DRAWER))
-        widgetsPrefsGraph(route = subRoute(Routes.PREFS_WIDGETS))
-        preferenceGraph(route = subRoute(Routes.PREFS_SEARCH), { SearchPrefsPage() })
-        preferenceGraph(route = subRoute(Routes.PREFS_BACKUPS), { BackupsPrefsPage() })
-        //preferenceGraph(route = subRoute(Routes.PREFS_DM), { BlankScreen() })
-        devPrefsGraph(route = subRoute(Routes.PREFS_DEV))
-        gesturesPrefsGraph(route = subRoute(Routes.PREFS_GESTURES))
-        aboutPrefsGraph(route = subRoute(Routes.ABOUT))
-        editIconGraph(route = subRoute(Routes.EDIT_ICON))
+                pageData.value?.let {
+                    AnimatedPane {
+                        when (it) {
+                            is NavRoute.Profile.IconShape
+                            -> IconShapePage()
+
+                            is NavRoute.Profile.AccentColor
+                            -> ColorSelectionPage(PrefKey.PROFILE_ACCENT_COLOR)
+
+                            is NavRoute.Profile
+                            -> ProfilePrefsPage()
+
+                            is NavRoute.Desktop.FolderBG
+                            -> ColorSelectionPage(PrefKey.DESKTOP_FOLDER_BG_COLOR)
+
+                            is NavRoute.Desktop.FolderStroke
+                            -> ColorSelectionPage(PrefKey.DESKTOP_FOLDER_STROKE_COLOR)
+
+                            is NavRoute.Desktop
+                            -> DesktopPrefsPage()
+
+                            is NavRoute.Dock.BG
+                            -> ColorSelectionPage(PrefKey.DOCK_BG_COLOR)
+
+                            is NavRoute.Dock
+                            -> DockPrefsPage()
+
+                            is NavRoute.Drawer.BG
+                            -> ColorSelectionPage(PrefKey.DRAWER_BG_COLOR)
+
+                            is NavRoute.Drawer.Categorize
+                            -> AppCategoriesPage()
+
+                            is NavRoute.Drawer.HiddenApps
+                            -> HiddenAppsPage()
+
+                            is NavRoute.Drawer.ProtectedApps
+                            -> ProtectedAppsPage()
+
+                            is NavRoute.Drawer.ProtectedAppsView
+                            -> ProtectedAppsView()
+
+                            is NavRoute.Drawer
+                            -> DrawerPrefsPage()
+
+                            is NavRoute.Widgets.NotificationDots
+                            -> ColorSelectionPage(PrefKey.NOTIFICATION_DOTS_COLOR)
+
+                            is NavRoute.Widgets
+                            -> WidgetsPrefsPage()
+
+                            is NavRoute.Search
+                            -> SearchPrefsPage()
+
+                            is NavRoute.Gestures.EditDash
+                            -> EditDashPage()
+
+                            is NavRoute.Gestures.Gesture
+                            -> GestureSelectorPage(gesturesMap[it.key]!!)
+
+                            is NavRoute.Gestures
+                            -> GesturesPrefsPage()
+
+                            is NavRoute.Backup
+                            -> BackupsPrefsPage()
+
+                            is NavRoute.Dev.Categorize
+                            -> AppCategoriesPage()
+
+                            is NavRoute.Dev
+                            -> DevPrefsPage()
+
+                            is NavRoute.About.License
+                            -> LicenseScreen()
+
+                            is NavRoute.About.Translators
+                            -> TranslatorsScreen()
+
+                            is NavRoute.About.Changelog
+                            -> ChangelogScreen()
+
+                            is NavRoute.About
+                            -> AboutPrefPage()
+
+                            else -> {}
+                        }
+                    }
+                }
+            }
+        )
     }
 }
