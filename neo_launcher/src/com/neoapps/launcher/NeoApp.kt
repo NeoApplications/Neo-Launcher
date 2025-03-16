68e5b85c6ae19d2761fae36fa23e92f266755d2f
saulhdev <saul_henriquez@hotmail.com>
2025-03-16 00:07:50 -0600

    Apply Koin modules
    Signed-off-by: saulhdev <saul_henriquez@hotmail.com>

--- neo_launcher/src/com/neoapps/launcher/NeoApp.kt
+++ neo_launcher/src/com/neoapps/launcher/NeoApp.kt
@@ -18,5 +18,81 @@
 package com.neoapps.launcher
 
 import android.app.Application
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.neoapps.launcher.preferences.NeoPrefs
+import com.neoapps.launcher.util.CoreUtils.Companion.minSDK
+import org.chickenhook.restrictionbypass.Unseal
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androix.startup.KoinStartup
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.koinConfiguration
+import org.koin.dsl.module
 
-class NeoApp: Application() {}
+@OptIn(KoinExperimentalAPI::class)
+class NeoApp : Application(), KoinStartup {
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+        name = "neo_launcher"
+    )
+
+    val prefsModule = module {
+        single { NeoPrefs(get()) }
+        single { androidContext().dataStore }
+    }
+
+    private val dataModule = module {
+    }
+    private val coreModule = module {
+        single { NeoLauncher.getLauncher(get()) }
+    }
+
+    val appModule = module {
+
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        startKoin {
+            androidLogger()
+            androidContext(this@NeoApp)
+            modules(coreModule, dataModule, appModule, prefsModule)
+        }
+        if (minSDK(Build.VERSION_CODES.P)) {
+            try {
+                Unseal.unseal()
+                Log.i(TAG, "Unseal success!")
+            } catch (e: Exception) {
+                Log.e(TAG, "Unseal fail!")
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onKoinStartup() = koinConfiguration {
+        androidLogger()
+        androidContext(this@NeoApp)
+        modules(coreModule, dataModule, appModule, prefsModule)
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        GlobalContext.get().close()
+    }
+
+    companion object {
+        @JvmStatic
+        var instance: NeoApp? = null
+            private set
+
+        private const val TAG = "OmegaApp"
+    }
+}
+
+val Context.neoApp get() = applicationContext as NeoApp
