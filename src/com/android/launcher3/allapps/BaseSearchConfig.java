db07947baa4c1ba8fab2cb1eecbe657575f49280
saulhdev <saul_henriquez@hotmail.com>
2025-06-14 18:57:55 -0600

    Remove animationlib and iconloaderlib from libs_systemui
    Signed-off-by: saulhdev <saul_henriquez@hotmail.com>

--- src/com/android/launcher3/allapps/BaseSearchConfig.java
+++ src/com/android/launcher3/allapps/BaseSearchConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,14 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package com.android.launcher3.util;
+package com.android.launcher3.allapps;
 
 /**
- * Extension of closeable which does not throw an exception
+ * Base config values for search.
  */
-public interface SafeCloseable extends AutoCloseable {
+public class BaseSearchConfig {
+    public BaseSearchConfig() {
+    }
 
-    @Override
-    void close();
+    /**
+     * Returns whether to enable the synchronized keyboard transition between Home and All Apps.
+     */
+    public boolean isKeyboardSyncEnabled() {
+        return false;
+    }
 }
