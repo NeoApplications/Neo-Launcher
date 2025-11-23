dc6b308822712f42d7c22f4a4f8866ace932b07f
Saul Henriquez <saul_henriquez@hotmail.com>
2025-11-22 21:40:01 -0600

    Add missing shared Flags
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ flags/src/com/android/systemui/shared/Flags.kt
@@ -0,0 +1,10 @@
+package com.android.systemui.shared
+
+object Flags {
+    fun extendibleThemeManager(): Boolean{
+        return false
+    }
+    fun newCustomizationPickerUi(): Boolean{
+        return true
+    }
+}
