5c6e518a919b308b813c3841102cfea5585b8673
saulhdev <saul_henriquez@hotmail.com>
2025-12-06 00:18:05 -0600

    Restore compatLib
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ compatLib/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    compileSdk = 36
+    namespace = "app.lawnchair.compatlib"
+
+    sourceSets {
+        getByName("main") {
+            kotlin.directories.add("src/main/java")
+            java.directories.add("src/main/java")
+            aidl.directories.add("src/main/java")
+        }
+    }
+
+    buildFeatures {
+        aidl = true
+    }
+
+}
+
+
+val FRAMEWORK_PREBUILTS_DIR = "$rootDir/prebuilt/libs"
+val addFrameworkJar = { name: String ->
+    val frameworkJar = File(FRAMEWORK_PREBUILTS_DIR, name)
+    if (!frameworkJar.exists()) {
+        throw IllegalArgumentException("Framework jar path ${frameworkJar.path} doesn't exist")
+    }
+    gradle.projectsEvaluated {
+        tasks.withType<JavaCompile>().configureEach {
+            classpath = files(frameworkJar, classpath)
+        }
+        tasks.withType<KotlinCompile>().configureEach {
+            libraries.setFrom(files(frameworkJar, libraries))
+        }
+    }
+    dependencies {
+        compileOnly(files(frameworkJar))
+    }
+}
+addFrameworkJar("framework-15.jar")
---
+++ compatLib/compatLibVS/src/main/java/app/lawnchair/compatlib/twelve/ActivityOptionsCompatVS.java
@@ -0,0 +1,9 @@
+package app.lawnchair.compatlib.twelve;
+
+import androidx.annotation.RequiresApi;
+
+import app.lawnchair.compatlib.eleven.ActivityOptionsCompatVR;
+
+@RequiresApi(31)
+public class ActivityOptionsCompatVS extends ActivityOptionsCompatVR {
+}
