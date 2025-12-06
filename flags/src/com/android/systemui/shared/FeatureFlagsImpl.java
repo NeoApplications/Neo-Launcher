693bd7855bef324d3f889ffc030c06a36a1a3725
saulhdev <saul_henriquez@hotmail.com>
2025-12-06 09:24:43 -0600

    Add missing files to flags project
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

---
+++ flags/src/com/android/systemui/shared/FeatureFlagsImpl.java
@@ -0,0 +1,174 @@
+package com.android.systemui.shared;
+// TODO(b/303773055): Remove the annotation after access issue is resolved.
+
+/** @hide */
+public final class FeatureFlagsImpl implements FeatureFlags {
+    @Override
+
+
+    public boolean ambientAod() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean bouncerAreaExclusion() {
+        return true;
+    }
+
+    @Override
+
+
+    public boolean clockReactiveSmartspaceLayout() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean clockReactiveVariants() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean cursorHotCorner() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean enableHomeDelay() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean enableLppSqueezeEffect() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean exampleSharedFlag() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean extendedWallpaperEffects() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean lockscreenCustomClocks() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean newCustomizationPickerUi() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean newTouchpadGesturesTutorial() {
+        return true;
+    }
+
+    @Override
+
+
+    public boolean returnAnimationFrameworkLibrary() {
+        return true;
+    }
+
+    @Override
+
+
+    public boolean returnAnimationFrameworkLongLived() {
+        return true;
+    }
+
+    @Override
+
+
+    public boolean screenshotContextUrl() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean shadeAllowBackGesture() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean sidefpsControllerRefactor() {
+        return true;
+    }
+
+    @Override
+
+
+    public boolean smartspaceRemoteviewsIntentHandler() {
+        return true;
+    }
+
+    @Override
+
+
+    public boolean smartspaceSportsCardBackground() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean smartspaceUiUpdate() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean smartspaceUiUpdateResources() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean statusBarConnectedDisplays() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean threeButtonCornerSwipe() {
+        return false;
+    }
+
+    @Override
+
+
+    public boolean usePreferredImageEditor() {
+        return false;
+    }
+
+}
