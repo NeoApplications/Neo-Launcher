7d10772b9df12cd95d668af11d4a9b7e11eb9ced
Saul Henriquez <saul_henriquez@hotmail.com>
2026-02-22 10:46:52 -0600

    Refactor EditDashPage to clean up commented code and improve layout structure

--- Omega/src/com/neoapps/neolauncher/compose/pages/preferences/EditDashPage.kt
+++ Omega/src/com/neoapps/neolauncher/compose/pages/preferences/EditDashPage.kt
@@ -17,20 +17,46 @@
  */
 package com.neoapps.neolauncher.compose.pages.preferences
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.android.launcher3.R
+import com.neoapps.neolauncher.compose.components.ListItemWithIcon
 import com.neoapps.neolauncher.compose.components.ViewWithActionBar
 import com.neoapps.neolauncher.compose.components.move
 import com.neoapps.neolauncher.dash.dashProviderOptions
 import com.neoapps.neolauncher.preferences.NeoPrefs
 import com.neoapps.neolauncher.preferences.iconIds
+import com.neoapps.neolauncher.theme.GroupItemShape
+import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyListState
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -69,7 +95,7 @@ fun EditDashPage() {
             dashProvidersItems.setAll(enabledKeys)
         }
     ) { paddingValues ->
-        /*LazyColumn(
+        LazyColumn(
             verticalArrangement = Arrangement.spacedBy(4.dp),
             modifier = Modifier
                 .fillMaxSize()
@@ -176,7 +202,7 @@ fun EditDashPage() {
             item {
                 Spacer(modifier = Modifier.height(8.dp))
             }
-        }*/
+        }
     }
 
     DisposableEffect(key1 = null) {
